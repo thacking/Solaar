a2310d7f2327a18ef8aa550890a384e9ddee5461
Lukas Holzner <git@lukas.lhal.de>
2025-11-22 02:26:09 +0100

    Added nix shall and flake

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "Solaar development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+          evdev
+          pyudev
+          pyyaml
+          xlib
+          psutil
+          dbus-python
+          pygobject3
+          typing-extensions
+          
+          # Dev dependencies
+          pytest
+          pytest-mock
+          pytest-cov
+          ruff
+        ]);
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.gobject-introspection
+            pkgs.wrapGAppsHook3
+          ];
+
+          buildInputs = [
+            pythonEnv
+            pkgs.gtk3
+            pkgs.libnotify
+            pkgs.udev
+            pkgs.gdk-pixbuf
+            pkgs.glib
+            pkgs.gsettings-desktop-schemas
+          ];
+
+          shellHook = ''
+            export PYTHONPATH=$PYTHONPATH:$(pwd)/lib
+            echo "Solaar development environment loaded."
+            echo "You can run solaar with: bin/solaar"
+            echo "If you need root permissions (e.g. for device access without udev rules), use: sudo -E bin/solaar"
+          '';
+        };
+      }
+    );
+}
